162c98aacd6a600a4789abb291ef8d797cc13676
Stephen Chen <chen.stephen.r@gmail.com>
2019-03-01 14:45:39 -0800

    get post and delete are functional

--- routes/api/items.js
+++ routes/api/items.js
@@ -24,4 +24,13 @@ router.post('/', (req, res) => {
   newItem.save().then(item => { res.json(item) });
 });
 
+// @route   DELETE api/items/:id
+// @desc    Delete an Item
+// @access  Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json(item)))
+    .catch(err => res.status(404).json({ success: false }));;
+});
+
 module.exports = router;
