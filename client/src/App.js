f7bcdcaf78700ba5da899816151478b874352252
Stephen Chen <chen.stephen.r@gmail.com>
2019-05-13 17:01:17 -0700

    connected db

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AppNavBar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -14,7 +16,10 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <AppNavBar />
-          <ShoppingList />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
         </div>
       </Provider>
     );
--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,14 +1,34 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import axios from 'axios';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
-export const getItems = () => {
-  return {
-    type: GET_ITEMS
-  };
+export const getItems = () => dispatch => {
+  dispatch(setItemsLoading());
+  axios
+    .get('/api/items')
+    .then(res => 
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data
+      })
+    );
 };
 
-export const deleteItem = (id) => {
+export const deleteItem = id => {
   return {
     type: DELETE_ITEM,
     payload: id
+  };
+};
+
+export const addItem = item => {
+  return {
+    type: ADD_ITEM,
+    payload: item
+  };
+};
+
+export const setItemsLoading = () => {
+  return {
+    type: ITEMS_LOADING
   };
 };
