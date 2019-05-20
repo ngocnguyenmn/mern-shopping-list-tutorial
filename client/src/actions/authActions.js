6363fb7dce2ff54ee1aac44711f7b204c6bf6336
Stephen Chen <chen.stephen.r@gmail.com>
2019-05-20 16:18:02 -0700

    created login modal (add prettier too)

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import { returnErrors } from './errorActions';
+import axios from "axios";
+import { returnErrors } from "./errorActions";
 
 import {
   USER_LOADED,
@@ -10,19 +10,22 @@ import {
   LOGOUT_SUCCESS,
   REGISTER_SUCCESS,
   REGISTER_FAIL
-} from './types';
-import Axios from 'axios';
+} from "./types";
+import Axios from "axios";
 
 // Check token & load user
 export const loadUser = () => (dispatch, getState) => {
   // User Loading
   dispatch({ type: USER_LOADING });
 
-  axios.get('/api/auth/user', tokenConfig(getState))
-    .then(res => dispatch({
-      type: USER_LOADED,
-      payload: res.data
-    }))
+  axios
+    .get("/api/auth/user", tokenConfig(getState))
+    .then(res =>
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      })
+    )
     .catch(err => {
       dispatch(returnErrors(err.response.data, err.response.status));
       dispatch({
@@ -36,25 +39,67 @@ export const register = ({ name, email, password }) => dispatch => {
   // Headers
   const config = {
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     }
-  }
+  };
 
   // Request body
   const body = JSON.stringify({ name, email, password });
 
-  axios.post('/api/users', body, config)
-    .then(res => dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data
-    }))
+  axios
+    .post("/api/users", body, config)
+    .then(res =>
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      })
+    )
     .catch(err => {
-      dispatch(returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL'));
+      dispatch(
+        returnErrors(err.response.data, err.response.status, "REGISTER_FAIL")
+      );
       dispatch({
         type: REGISTER_FAIL
+      });
+    });
+};
+
+// Login User
+export const login = ({ email, password }) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  // Request body
+  const body = JSON.stringify({ email, password });
+
+  axios
+    .post("/api/auth", body, config)
+    .then(res =>
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
       })
-    })
-}
+    )
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, "LOGIN_FAIL")
+      );
+      dispatch({
+        type: LOGIN_FAIL
+      });
+    });
+};
+
+// Logout User
+export const logout = () => {
+  return {
+    type: LOGOUT_SUCCESS
+  };
+};
 
 // Setup config/headers and token
 export const tokenConfig = getState => {
@@ -70,8 +115,8 @@ export const tokenConfig = getState => {
 
   // If token, add to headers
   if (token) {
-    config.headers['x-auth-token'] = token;
+    config.headers["x-auth-token"] = token;
   }
 
   return config;
-}
+};
--- client/src/components/auth/Logout.js
+++ client/src/components/auth/Logout.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { NavLink } from "reactstrap";
 import { connect } from "react-redux";
 import { logout } from "../../actions/authActions";
+import { PropTypes } from "prop-types";
 
 export class Logout extends Component {
   static propTyeps = {
